de8b056d74e2cf00dd9878b90717b64575d4eefa
jay-j <jay-j@github.com>
2021-10-31 23:38:09 -0500

    working! kp is crappy but it does seem to work

--- main.cpp
+++ main.cpp
@@ -6,9 +6,6 @@
 #include "mjgraphics.hpp"
 
 #include "stdio.h"
-//#include "stdlib.h"
-//#include "string.h"
-//#include "math.h"
 #include "assert.h"
 
 #include "lapacke.h"
@@ -18,7 +15,22 @@ mjModel* m = NULL;                  // MuJoCo model
 mjData* d = NULL;                   // MuJoCo data
 GUI gui;                            // window and UI elements
 int simulation_exit;                // 0 to continue, 1 to call to exit loop
+int simulation_mode;                // various per the defines
+int simulation_step_frames;         // counter to track/limit progress in step mode
+#define MODE_SIM 0
+#define MODE_INSPECT 1
+#define MODE_STEP 2
 
+void mode_switch_inspect(){
+    simulation_mode = MODE_INSPECT;
+}
+void mode_switch_step(){
+    simulation_mode = MODE_STEP;
+    simulation_step_frames = 0;
+}
+void mode_switch_resume(){
+    simulation_mode = MODE_SIM;
+}
 
 // keyboard callback
 void keyboard(GLFWwindow* window, int key, int scancode, int act, int mods){
@@ -33,7 +45,30 @@ void keyboard(GLFWwindow* window, int key, int scancode, int act, int mods){
     if (key == GLFW_KEY_ESCAPE){
         simulation_exit = 1;
     }
+    if (key == GLFW_KEY_F1){
+        mode_switch_inspect();
+    }
+    if (key == GLFW_KEY_F2){
+        mode_switch_step();
+    }
+    if (key == GLFW_KEY_F3){
+        mode_switch_resume();
+    }
 
+}
+
+
+void identity(mjtNum* result, size_t size){ // TODO more efficient or not every loop
+    for (size_t i=0; i<size; i++){
+        for(size_t j=0; j<size; j++){
+            if (i == j){
+                result[i*size + j] = 1;
+            }
+            else{
+                result[i*size + j] = 0;
+            }
+        }
+    }
 }
 
 
@@ -65,18 +100,15 @@ void pseudo_inverse(double* result, double* matrix, int nr, int nc){
     double rcond = -1;
     int rank;
 
-    int lwork = 128; // TODO WAG a big area, figure it out properly later
-    double work[lwork];
-
     int matrix_layout = LAPACK_ROW_MAJOR;
     int info = LAPACKE_dgelss(matrix_layout, nr, nc, nrhs, A, lda, B, ldb, S, rcond, &rank);
 
     if (info < 0){
-        printf("if INFO = -i, the i-th argument had an illegal value. info = %d\n", info);
+        printf("If INFO = -i, the i-th argument had an illegal value. info = %d\n", info);
         assert(0);
     }
     if (info > 0){
-        printf(" the algorithm for computing the SVD failed to converge; if INFO = i, i off-diagonal elements of an intermediate bidiagonal form did not converge to zero. info = %d\n", info);
+        printf("The algorithm for computing the SVD failed to converge; if INFO = i, i off-diagonal elements of an intermediate bidiagonal form did not converge to zero. info = %d\n", info);
         assert(0);
     }
 
@@ -97,14 +129,14 @@ void controller(const mjModel* m, mjData* d){
     mjtNum jacr[3*m->nv];
     mj_jac(m, d, jacp, jacr, ee_pos, ee_body_id);
 
-    printf("Unconstrained Jacobian JacP:\n");
-    mju_printMat(jacp, 3, m->nv);
-    printf("Unconstrained Jacobian JacR:\n");
-    mju_printMat(jacr, 3, m->nv);
+    // printf("Unconstrained Jacobian JacP:\n");
+    // mju_printMat(jacp, 3, m->nv);
+    // printf("Unconstrained Jacobian JacR:\n");
+    // mju_printMat(jacr, 3, m->nv);
 
     // get the raw constraint jacobian A
-    printf("Raw constraint jacobian d->efc_J:\n");
-    mju_printMat(d->efc_J, m->njmax, m->nv);
+    // printf("Raw constraint jacobian d->efc_J:\n");
+    // mju_printMat(d->efc_J, m->njmax, m->nv);
 
     // get the active part of d->efc_J, grabbing all rows that have nonzero entries
     mjtNum efc_J_active[m->nv * m->njmax]; // make it bigger than it needs to be
@@ -128,9 +160,8 @@ void controller(const mjModel* m, mjData* d){
         }
     }
 
-
-    printf("truncated efc_J:\n");
-    mju_printMat(efc_J_active, njused, m->nv); // (njused x m->nv)
+    // printf("truncated efc_J:\n");
+    // mju_printMat(efc_J_active, njused, m->nv); // (njused x m->nv)
 
     // calculate A' (A transpose)
     mjtNum efc_JT[njused*m->nv]; // create variable for JT since in dense mode it is not automatically created
@@ -139,29 +170,134 @@ void controller(const mjModel* m, mjData* d){
     // variable to store A * A'
     mjtNum AAT[njused*njused];
     mju_mulMatMat(AAT, d->efc_J, efc_JT, njused, m->nv, njused);
-    printf("Need to invert this matrix: A * A'\n");
-    mju_printMat(AAT, njused, njused);
+    // printf("Need to invert this matrix: A * A'\n");
+    // mju_printMat(AAT, njused, njused);
 
     mjtNum inv_AAT[njused*njused];
-
     pseudo_inverse(inv_AAT, AAT, njused, njused);
-
-    printf("Inverted matrix inv_AAT:\n");
-    mju_printMat(inv_AAT, njused, njused);
+    // printf("Inverted matrix inv_AAT:\n");
+    // mju_printMat(inv_AAT, njused, njused);
 
     // finish calculations of constraint correction matrix
+    // invAAT * A : (njused x njused) * (njused x m->nv) = (njused x m->nv)
+    mjtNum tmp1[njused*m->nv];
+    mju_mulMatMat(tmp1, inv_AAT, efc_J_active, njused, njused, m->nv);
 
+    // A' * tmp1 : (m->nv x njused) * (njused x m->nv) = (m->nv x m->nv)
+    mjtNum tmp2[m->nv*m->nv];
+    mju_mulMatMat(tmp2, efc_JT, tmp1, m->nv, njused, m->nv);
+
+    mjtNum eye[m->nv*m->nv];
+    identity(eye, m->nv);
+    mjtNum constraint[m->nv*m->nv];
+    mju_sub(constraint, eye, tmp2, m->nv*m->nv);
+
+    // printf("Jacobian Adjustment Matrix:\n");
+    // mju_printMat(constraint, m->nv, m->nv);
 
     // combine with unconstrain jacobian - get the constrained jacobian!
+    // J * constraint : (3 x m->nv) * (m->nv x m->nv) = (3 x m->nv)
+    mjtNum jacobian_mod[6*m->nv];
+    mju_mulMatMat(jacobian_mod, jacp, constraint, 3, m->nv, m->nv);
+    mju_mulMatMat(jacobian_mod+(3*m->nv), jacr, constraint, 3, m->nv, m->nv);
 
+    // printf("modified jacoban:\n");
+    // mju_printMat(jacobian_mod, 6, m->nv);
+
+    // given actuators, compute which DOF they correspond to
+    int actuator_qty = 3; // TODO non-generic
+    int actuator_id[actuator_qty]; 
+    int dof_actuated[3]; // TODO non-generic
+    actuator_id[0] = mj_name2id(m, mjOBJ_ACTUATOR, "act_leg0_joint0");
+    dof_actuated[0] = m->jnt_dofadr[m->actuator_trnid[2*actuator_id[0]]];
+
+    actuator_id[1] = mj_name2id(m, mjOBJ_ACTUATOR, "act_leg1_joint0");
+    dof_actuated[1] = m->jnt_dofadr[m->actuator_trnid[2*actuator_id[1]]];
+
+    actuator_id[2] = mj_name2id(m, mjOBJ_ACTUATOR, "act_leg2_joint0");
+    dof_actuated[2] = m->jnt_dofadr[m->actuator_trnid[2*actuator_id[2]]];
+
+    printf("dof actuated: %d, %d, %d\n", dof_actuated[0], dof_actuated[1], dof_actuated[2]);
+
+    int axis[6];
+    axis[0] = 1; // want to control x position
+    axis[1] = 1; // want to control y position
+    axis[2] = 0;
+    axis[3] = 0;
+    axis[4] = 0;
+    axis[5] = 1; // want to control z rotation
+
+    mjtNum rot[9]; // rotation of the base - for commands relative to a mobile robot base
+    identity(rot, 3);
+    //mju_copy(rot, &(d->xmat[9*ee_body_id]), 9);
 
     // truncate constrained jacobian according to the directions we want to control and the actuators we have available to do it
+    mjtNum jacobian_active[3*3]; // TODO generic
+    for (size_t i=0; i<actuator_qty; ++i){  // loop over actuators
+        mjtNum jacobian_column_global[6]; 
 
+        for (size_t j=0; j<6; ++j){
+            jacobian_column_global[j] = jacobian_mod[j*m->nv + dof_actuated[i]];
+        }
 
-    // use the jacobian
-    // d->ctrl[]
+        mjtNum jacobian_column_local[6];
+        mju_transformSpatial(jacobian_column_local, jacobian_column_global, 0, ee_pos, ee_pos, rot); // TODO point old..
 
+        // copy data in jacobian_active - for only the axis we care about
+        int k = 0;
+        for(size_t j=0; j<6; j++){
+            if (axis[j] == 1){
+                jacobian_active[k*actuator_qty + i] = jacobian_column_local[j];
+                ++k;
+            }
+        }
+    }
 
+    printf("Now reduced to the active jacobian:\n");
+    mju_printMat(jacobian_active, 3, 3);
+
+    mjtNum jacobian_active_T[3*3]; // TODO generic size
+    mju_transpose(jacobian_active_T, jacobian_active, 3, 3); // TODO generic size
+
+    // TODO a bunch of relative position error calculating stuff!!!!
+    mjtNum state_target[3];
+    state_target[0] = 0.1*cos(0.05*d->time);
+    state_target[1] = 0.1*sin(0.05*d->time);
+    state_target[2] = 0;
+
+    mjtNum state_current[3];
+    state_current[0] = d->xpos[3*ee_body_id];
+    state_current[1] = d->xpos[3*ee_body_id+1];
+
+    mjtNum state_target_quat[4];
+    state_target_quat[0] = 1;
+    state_target_quat[1] = 0;
+    state_target_quat[2] = 0;
+    state_target_quat[3] = 0;
+
+    mjtNum state_error_quat[3];
+    mju_subQuat(state_error_quat, state_target_quat, &(d->xquat[4*ee_body_id]));
+    printf("quat error: %lf, %lf, %lf\n", state_error_quat[0], state_error_quat[1], state_error_quat[2]);
+
+    state_current[2] = 2*state_error_quat[2]; // TODO is this what I want?
+
+    mjtNum desired_velocity_cart[3];
+    mju_sub3(desired_velocity_cart, state_target, state_current); // TODO better error for angles, generic size
+    desired_velocity_cart[2] *= 0.05; // TODO is there a more sensible way to control angular axis proportions? auto based on type
+    printf("Desired Velocity Cart: %lf, %lf, %lf\n", desired_velocity_cart[0], desired_velocity_cart[1], desired_velocity_cart[2]);
+
+    mjtNum desired_velocity_joint[actuator_qty];
+    mju_mulMatVec(desired_velocity_joint, jacobian_active_T, desired_velocity_cart, actuator_qty, 3); // todo generic axis qty
+
+    // use the jacobian, very simple proportional controller
+    for (size_t i=0; i<actuator_qty; ++i){
+        // get current location of actuator
+        mjtNum loc = d->actuator_length[actuator_id[i]];
+
+        // increment using desired joint velocity calc
+        d->ctrl[actuator_id[i]] = loc + 5*desired_velocity_joint[i];
+        printf("cmd: %lf  vel: %lf\n", d->ctrl[actuator_id[i]],  desired_velocity_joint[i]);
+    }
 }
 
 
@@ -186,19 +322,30 @@ int main(int argc, const char** argv){
 
     // run main loop, target real-time simulation and 60 fps rendering
     while( (!glfwWindowShouldClose(gui.window)) && (simulation_exit == 0) ){
-        mjtNum simstart = d->time;
-        while( d->time - simstart < 1.0/60.0 ){
-            // calculate as much as possible
-            mj_kinematics(m, d);
-            mj_forward(m, d);
 
-            // run the controller on the parallel mechanism
-            controller(m, d);
+        if ((simulation_mode == MODE_SIM) || (simulation_mode == MODE_STEP)){
 
-            // finally, advance time
-            mj_step(m, d);
+            mjtNum simstart = d->time;
+            while( d->time - simstart < 1.0/60.0 ){
+                // calculate as much as possible
+                mj_kinematics(m, d);
+                mj_forward(m, d);
+
+                // run the controller on the parallel mechanism
+                controller(m, d);
+
+                // finally, advance time
+                mj_step(m, d);
+            }
+
+            // if in step mode, pause after a couple frames
+            simulation_step_frames++;
+            if (simulation_mode == MODE_STEP){
+                if (simulation_step_frames > 1){
+                    simulation_mode = MODE_INSPECT;
+                }
+            }
         }
-            
         graphics_draw(m, d);
     }
     
